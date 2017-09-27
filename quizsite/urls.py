0b8199226dfce7e742eef202517b6e75df5ad7c0
sofie_matilda <sofie.nilsson@doberman.se>
2017-09-27 14:43:40 +0200

    #17 Linking between pages

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from quiz import views
 
 urlpatterns = [
-    path("", views.startpage),
-    path("quiz/<int:quiz_number>/", views.quiz),
-    path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
-    path("quiz/<int:quiz_number>/completed/", views.completed),
+    path("", views.startpage, name="start_page"),
+    path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
+    path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
+    path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),
 ]
